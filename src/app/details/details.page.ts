d82f1b81474129f77e075664b83b4f5251324732
Ken Sodemann <ken@ionic.io>
2019-09-03 16:18:03 -0500

    manually set focus

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage implements AfterViewInit {
+  @ViewChild(IonInput, { static: true }) firstInput;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log('after view init the details view', this.firstInput);
+    this.firstInput.setFocus();
   }
-
 }
